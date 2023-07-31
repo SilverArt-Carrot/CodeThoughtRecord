02117fc95a86aab139e73bc7b7b2dfa83e31cec5
luoqy <luoqy@taofen8.com>
2023-07-31 18:11:10 +0800

    up

--- src/main/java/leet/LeetCode.java
+++ src/main/java/leet/LeetCode.java
@@ -240,7 +240,7 @@ public class LeetCode {
             Arrays.sort(chars);
             String s = new String(chars);
 
-            int hashCode = s.hashCode();
+            int hashCode = s.hashCode();  // 多余的，put进去的string key本身就是计算了hashcode
             List<String> list = map.computeIfAbsent(hashCode, k -> new ArrayList<>());
 
             list.add(str);
@@ -251,6 +251,125 @@ public class LeetCode {
         }
         return result;
     }
+
+    /**
+     * 128. 最长连续序列
+     * 中等
+     * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+     *
+     * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [100,4,200,1,3,2]
+     * 输出：4
+     * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+     * 示例 2：
+     *
+     * 输入：nums = [0,3,7,2,5,8,4,6,0,1]
+     * 输出：9
+     */
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int max = 0;
+        for (int num : nums) {
+            if (set.contains(num - 1)) {
+                continue;
+            }
+            int length = 1;
+            int t = num + 1;
+            while (set.contains(t)) {
+                length++;
+                t++;
+            }
+            max = Math.max(max, length);
+        }
+        return max;
+    }
+    public static int longestConsecutive2(int[] nums) {  // hash版，说是动态规划，我太笨看不出来，很nb就完事了
+        int max = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                // 左边的
+                int left = map.getOrDefault(num - 1, 0);
+                // 右边的
+                int right = map.getOrDefault(num + 1, 0);
+
+                int length = 1 + left + right;
+                if (length > max) {
+                    max = length;
+                }
+
+                // 左右都没有也不影响，加减0了
+                map.put(num - left, length);  // 将端点设置值，因为之后出现的数字也只会寻找端点
+                map.put(num + right, length);
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 121. 买卖股票的最佳时机
+     * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+     *
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     *
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     * 示例 1：
+     *
+     * 输入：[7,1,5,3,6,4]
+     * 输出：5
+     * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+     * 示例 2：
+     * 输入：prices = [7,6,4,3,1]
+     * 输出：0
+     * 解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+     */
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = Math.min(price, minPrice);
+            maxProfit = Math.max(price - minPrice, maxProfit);
+        }
+        return maxProfit;
+        // int minprice = Integer.MAX_VALUE;
+        // int maxprofit = 0;
+        // for (int i = 0; i < prices.length; i++) {
+        //     if (prices[i] < minprice) {
+        //         minprice = prices[i];
+        //     } else if (prices[i] - minprice > maxprofit) {
+        //         maxprofit = prices[i] - minprice;
+        //     }
+        // }
+        // return maxprofit;
+    }
+
+    /**
+     * 283. 移动零
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     *
+     * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+     * 示例 1:
+     *
+     * 输入: nums = [0,1,0,3,12]
+     * 输出: [1,3,12,0,0]
+     * 示例 2:
+     *
+     * 输入: nums = [0]
+     * 输出: [0]
+     */
+    public void moveZeroes(int[] nums) {
+
+    }
+
 
     public static void main(String[] args) {
 //        LeetCode leetCode = new LeetCode();
@@ -279,12 +398,30 @@ public class LeetCode {
 //            System.out.println();
 //        }
 
-        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+//        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+//
+//        List<List<String>> lists = groupAnagrams(strs);
+//
+//        for (List<String> list : lists) {
+//            System.out.println(list);
+//        }
+        // HashTable 不能有null的key 和 value，HashMap 可以
 
-        List<List<String>> lists = groupAnagrams(strs);
+//        Map<String, String> map = new HashMap<>();
+//        String put = map.put(null, "1");  // put是老值，没有就是null
+//        System.out.println(map.get(null));
+//        System.out.println(put);
+//
+//        String put1 = map.put("1", null);
+//        System.out.println(map.get("1"));
+//        System.out.println(put1);
 
-        for (List<String> list : lists) {
-            System.out.println(list);
-        }
+//        int[] nums = new int[]{0,3,7,2,5,8,4,6,0,1};
+//        int i = longestConsecutive(nums);
+//        System.out.println(i);
+
+//        int[] nums = new int[]{100,4,200,1,3,2};
+//        int i = longestConsecutive2(nums);
+//        System.out.println(i);
     }
 }
