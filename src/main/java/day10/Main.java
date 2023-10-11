74bd286ad749884ed7ff91ab75d1b726732772d4
luoqy <luoqy@taofen8.com>
2023-10-11 11:34:54 +0800

    update

--- src/main/java/day10/Main.java
+++ src/main/java/day10/Main.java
@@ -435,4 +435,28 @@ public class Main {
         }
         return dp[target];
     }
+
+    /**
+     * 一和零
+     */
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (String str : strs) {
+            int zeroNum = 0;
+            int oneNum = 0;
+            for (char c : str.toCharArray()) {
+                if (c == '1') {
+                    oneNum++;
+                } else {
+                    zeroNum++;
+                }
+            }
+            for (int i = m; i >= zeroNum; i--) {
+                for (int j = n; j >= oneNum; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeroNum][j - oneNum] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
 }
