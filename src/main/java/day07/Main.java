05429430cb8188b0432660768411a9a1c5fd8bb3
luoqy <luoqy@taofen8.com>
2023-10-26 11:54:19 +0800

    update

--- src/main/java/day07/Main.java
+++ src/main/java/day07/Main.java
@@ -960,5 +960,28 @@ public class Main {
         traverseBST(root.right);
     }
 
+    /**
+     * 二叉树的最近公共祖先
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
 
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else if (right != null) {
+            return right;
+        } else {
+            return null;
+        }
+    }
 }
